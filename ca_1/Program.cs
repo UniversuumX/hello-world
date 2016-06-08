fe327144708d4e3358fd11a75a8dc827409286b2
universuumx <universuumx@gmail.com>
2016-06-08 12:48:22 +0200

    news

--- ca_1/Program.cs
+++ ca_1/Program.cs
@@ -16,11 +16,13 @@ namespace ca_1
             DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
             Console.WriteLine("{0} --> Unix Seconds: {1}", dto, dto.ToUnixTimeSeconds());
 
-            dto = new DateTimeOffset(1969, 12, 31, 23, 59, 0, TimeSpan.Zero);
-            Console.WriteLine("{0} --> Unix Seconds: {1}", dto, dto.ToUnixTimeSeconds());
+            DateTimeOffset localDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(1465382055).DateTime.ToLocalTime();
+            Console.WriteLine("{0} --> Date time: {1}", 1465382055, localDateTimeOffset);
+            //dto = new DateTimeOffset(1969, 12, 31, 23, 59, 0, TimeSpan.Zero);
+            //Console.WriteLine("{0} --> Unix Seconds: {1}", dto, dto.ToUnixTimeSeconds());
 
-            dto = new DateTimeOffset(1970, 1, 1, 0, 1, 0, TimeSpan.Zero);
-            Console.WriteLine("{0} --> Unix Seconds: {1}", dto, dto.ToUnixTimeSeconds());
+            //dto = new DateTimeOffset(1970, 1, 1, 0, 1, 0, TimeSpan.Zero);
+            //Console.WriteLine("{0} --> Unix Seconds: {1}", dto, dto.ToUnixTimeSeconds());
 
             //Console.ReadLine();
         }
